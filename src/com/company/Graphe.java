4918c0f23514239955e3094b52a5f92be6b3dcf0
Ginks <pissabruney@hotmail.com>
2018-11-10 08:32:14 +0100

    Update Graphe.java

--- src/com/company/Graphe.java
+++ src/com/company/Graphe.java
@@ -1,146 +1,106 @@
 package com.company;
 
-import java.util.*;
-
-/**
- * Algotithme de Graphe
- */
-public class Graphe {
+public class Graphe implements Grille {
     protected final int n;
-    protected ArrayList<Arete> a;
+    protected ArrayList<LinkedList<Arete>> a;
+    protected static final int INFINITE= Integer.MAX_VALUE/2;
 
-    /**
-     * Constructeur de Graphe
-     * @param n Nombre de sommets
-     */
+
     public Graphe(int n) {
         this.n = n;
-        this.a =  new ArrayList<Arete>();
+        this.a =  new ArrayList<LinkedList<Arete>>();
         for(int i =0 ; i<n; i++){
-            a.add(new Arete(new LinkedList<Integer>(),0));
+            a.add(new LinkedList<Arete>());
         }
     }
 
-    /**
-     * Ajouter une arête au Graphe G
-     * @param i 1er Sommet
-     * @param j 2eme Sommet
-     */
     public void ajourteArete(int i, int j){
         if(verifieSommet(i)){
             if(verifieSommet(j)){
-                this.a.get(i).getAdj().add(j);
-                this.a.get(j).getAdj().add(i);
+                this.a.get(i).add(new Arete(j));
+                this.a.get(j).add(new Arete(i));
             }
         }
     }
 
-    /**
-     * Ajouter une arête valuée au Graphe G
-     * @param i 1er Sommet
-     * @param j 2eme Sommet
-     * @param poids Poids de l'arête
-     */
-    public void ajourteArete(int i, int j, int poids){
+    public void ajourteArete(int i, int j, int p){
         if(verifieSommet(i)){
             if(verifieSommet(j)){
-                this.a.get(i).getAdj().add(j);
-                this.a.get(j).getAdj().add(i);
-                this.a.get(i).setPoids(poids);
-                this.a.get(j).setPoids(poids);
-
+                this.a.get(i).add(new Arete(j,p));
+                this.a.get(j).add(new Arete(i,p));
             }
         }
     }
 
-    /**
-     * Verifier la présence d'un sommet dans le Graphe G
-     * @param v Sommet à vérifier
-     * @return présence du sommet
-     */
+
+
     public boolean verifieSommet(int v){
-        return v < this.getN() && v >= 0;
+        if(v>=this.getN()||v<0)
+            return false;
+        return true;
     }
 
-    /**
-     * Longueur du Graphe G
-     * @return n
-     */
     public int getN(){
         return this.n;
     }
 
-    /**
-     * Liste des voisins d'un Sommet
-     * @param v Un sommet
-     * @return Liste de voisins
-     */
-    public List<Integer> voisins (int v){
-        if(verifieSommet(v))
-            return Collections.unmodifiableList(this.a.get(v).getAdj());
-        return null;
-    }
-
-
-    /**
-     * Vérifier si deux sommets sont voisins
-     * @param i 1er sommet
-     * @param j 2eme sommet
-     * @return booléen
-     */
-    public boolean sontVoisins(int i, int j){
-        return (this.a.get(i).getAdj().contains(j)||this.a.get(j).getAdj().contains(i));
-    }
-
     public int degre(int v){
-        return this.a.get(v).getAdj().size();
+        return this.a.get(v).size();
     }
 
     public static int nbConnexesGraphes(int n) {
         if (n <= 0)
             return 0;
-        return (int) Math.pow(2, new Double((n * (n - 1)) / 2));
+        return (int) Math.pow(2, ((n * (n - 1)) / 2));
     }
 
 
-    /**
-     * Parcours en largeur sur tous les sommets d'un graphe
-     * @return Resultat du parcours en largeur
-     */
-    public Resultat parcoursLargeur(){
-        Resultat res = new Resultat((new String [this.getN()]),(new int [this.getN()]),(new int [this.getN()]));
+    public List<Arete> voisins (int v){
+        if(verifieSommet(v))
+            return Collections.unmodifiableList(this.a.get(v));
+        return null;
+    }
+
+    public boolean sontVoisins(int i, int j){
+        return (this.a.get(i).contains(j)||this.a.get(j).contains(i));
+    }
+
+
+    public Resultat parcoursLargeur(int v){
+        Resultat res = new Resultat((new String [this.n]),(new int [this.n]),(new int [this.n]));
         String s="";
         //Initialisation des tableaux Couleurs, Pi et Dist
-        for(int i=0; i<this.getN(); i++){
-            res.getCouleurs()[i]="blanc";
-            res.getPi()[i]=-1;
-            res.getDist()[i]=0;
+        for(int i=0; i<this.n; i++){
+            if(i==v){
+                res.getCouleurs()[v]="gris";
+                res.getPi()[v]=-1;
+                res.getDist()[v]=0;
+            }
+            else{
+                res.getCouleurs()[i]="blanc";
+                res.getPi()[i]=-1;
+                res.getDist()[i]=0;
+            }
+
         }
         //Parcours en largeur sur chaque sommet du Graphe G
-        for (int j = 0; j <this.getN() ; j++) {
+        for (int j = 0; j <this.n ; j++) {
             this.pl(j,res);
         }
         return res;
     }
 
-    /**
-     * Parcours en largeur sur un sommet v
-     * @param v un sommet de G
-     * @param res Resultat du parcours en largeur
-     */
+
     public void pl(int v, Resultat res){
         Stack<Integer> f= new Stack<Integer>(); //La file vide
-        if(res.getCouleurs()[v].equals("blanc")){
-            res.getCouleurs()[v]="gris";
-            res.getPi()[v]=-1;
-            res.getDist()[v]=0;
-        }
         f.add(v);
         int x=-1;
+        int i=0;
         while(!f.empty()){
             x=f.pop();
-            for (int i:this.a.get(x).getAdj()
-                 ) {
+            for (Arete a:this.a.get(x)
+                    ) {
+                i=a.getN();
                 if(res.getCouleurs()[i].equals("blanc")){
                     res.getCouleurs()[i]="gris";
                     res.getDist()[i]=res.getDist()[x]+1;
@@ -152,15 +112,17 @@ public class Graphe {
         res.getCouleurs()[x]="noir";
     }
 
+
     public void pp(int v, Resultat res){
         res.getCouleurs()[v]="gris";
         res.setTemps(res.getTemps()+1);
         res.getD()[v]=res.getTemps();
         for (int i = 0; i <this.getN() ; i++) {
-            if(this.sontVoisins(v,i)){
-                if(res.getCouleurs()[i].equals("blanc")){
-                    res.getPi()[i]=v;
-                    this.pp(i,res);
+            for (Arete av:this.a.get(i)
+                 ) {
+                if(res.getCouleurs()[av.getN()].equals("blanc")){
+                    res.getPi()[av.getN()]=v;
+                    this.pp(av.getN(),res);
                 }
             }
         }
@@ -190,30 +152,69 @@ public class Graphe {
     public String printArc(int i, int j){
         return  "(q"+i+"->q"+j+")";
     }
+    public String printArc(int i, int j, int p){
+        return  "(q"+i+"->q"+j+","+p+")";
+    }
 
     public String toString() {
         String s="A= {";
         for(int i = 0; i<this.n; i++){
-            for (Integer num : this.a.get(i).getAdj()
+            for (Arete num : this.a.get(i)
                     ) {
-                s+= this.printArc(i, num)+" ";
+                s+= this.printArc(i, num.getN(), num.getP())+" ";
             }
         }
         s+="}";
         return s;
     }
 
-    public String plusCourtsChemin(int w) {
-        Resultat res = this.parcoursLargeur();
-        String s = "" + w + "->";
-        int i, j = w;
-        while (j != -1) {
-            i = res.getPi()[j];
-            j = i;
-            s += i + "->";
+
+
+    public Resultat bellmanFord(int s){
+        Resultat res = new Resultat(new int[this.n], new int[this.n], true);
+        for(int i=0; i<this.n; i++){
+            res.getPi()[i]=-1;
+            if(i==s)
+                res.getD()[i]=0;
+            else
+                res.getD()[i]=INFINITE;
         }
+
+        for(int u=0; u<this.n; u++){
+            for (Arete av : this.a.get(u)){
+                if(res.getD()[av.getN()]>res.getD()[u]+ av.getP()){
+                    res.getD()[av.getN()]=res.getD()[u]+av.getP();
+                    res.getPi()[av.getN()]=u;
+                }
+            }
+        }
+        for (int u = 0; u <this.n ; u++) {
+            for (Arete av : this.a.get(u)){
+                if(res.getD()[av.getN()]>res.getD()[u]+ av.getP()){
+                    res.bool = false;
+                    return res;
+                }
+            }
+        }
+        res.bool=true;
+        return res;
+    }
+
+
+    public String plusCourtsChemin(int w, Resultat res) {
+        String s = "" + w + "->";
+        int i, j = 0;
+        while (w != -1) {
+            i = res.getPi()[w];
+            w = i;
+            s += i + "->";
+            j++;
+        }
+        System.out.println("Taille du plus court chemin = "+ (j-1));
         return s;
     }
+
+
 
     /***
      * Matrice d'adjacence du graphe
@@ -274,14 +275,14 @@ public class Graphe {
      */
     private boolean testCouleur(int s, String c, Boolean bool, String[] coul) {
         coul[s] = c;
-        for (Integer u : this.a.get(s).getAdj()
+        for (Arete u : this.a.get(s)
                 ) {
-            if (coul[u].equals("blanc")) {
+            if (coul[u.getN()].equals("blanc")) {
                 if (coul[s].equals("rouge"))
-                    this.testCouleur(u, "vert", bool, coul);
+                    this.testCouleur(u.getN(), "vert", bool, coul);
                 else
-                    this.testCouleur(u, "rouge", bool, coul);
-            } else if (coul[u].equals(coul[s]))
+                    this.testCouleur(u.getN(), "rouge", bool, coul);
+            } else if (coul[u.getN()].equals(coul[s]))
                 return false;
         }
         return true;
@@ -320,51 +321,39 @@ public class Graphe {
     }
 
 
-    /////////////  Graphes valués  /////////////
+    /////////////////////////////// Grille ////////////////////////////////////////////
+    /**
+     * Construire unr grille
+     *
+     * @param h hauteur
+     * @param l largeur
+     */
+    @Override
+    public void construireGrille(int h, int l) {
+        for (int i = 0; i <h ; i++) {
+            for (int j = 0; j <l ; j++) {
+                if(j<l&& i!=j)
+                    this.ajourteArete(i,j);
+                if(i<h && i!=j)
+                    this.ajourteArete(i,i+1);
+            }
+
+        }
+    }
 
     /**
-     * Détermine si le Graphe G est connexe
+     * Ajouter un mur entre i et j
      *
-     * @return Valeur vérité
+     * @param i case i
+     * @param j case j
      */
-    public boolean connexe() {
-        Resultat res = this.parcoursProfondeur();
-        int j = 0;
-        for (int i = 0; i < res.getPi().length; i++) {
-            if (res.getPi()[i] == -1)
-                j += 1;
-            if (j == 2)
-                return false;
-        }
-        return true;
+    @Override
+    public void ajouterMur(int i, int j) {
+
     }
 
+    ///////////////////////////// Grille ////////////////////////////////////////////////
 
-    ////////////////////////////////////////////
 
-
-    ///////////// Formule de récurrence ///////
-    //Nombre de graphes connexes à n sommets
-
-    public Resultat bellmanFord(int s) {
-        Resultat res = new Resultat(new int[this.n], new int[this.n], true);
-        for (int i = 0; i < this.n; i++) {  //Pour chaque i de S faire
-            res.getPi()[i] = -1; // pi de i = nil
-            if (i == s) // d de i
-                res.getDist()[i] = 0;
-            else // Sinon
-                res.getDist()[i] = -1;
-        }
-
-        for (int i = 0; i < this.n; i++) { //Pour i = 0 à |S| - 1 faire
-            for (Integer v : this.a.get(i).getAdj()
-                    ) {
-                if (res.getDist()[v] > res.getDist()[s]) {
-
-                }
-            }
-        }
-        return res;
-    }
 
 }
